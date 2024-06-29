dfd5f177361b3d8c2d5e88577d34d99d4d7d3702
iemong <kiyoshidainagon@gmail.com>
2024-06-29 23:21:48 +0900

    add

--- src/lib/RSSList.ts
+++ src/lib/RSSList.ts
@@ -1,4 +1,5 @@
 export const RSSList = new Map<string, string>([
 	['jser.info', 'https://jser.info/rss/'],
 	['azukiazusa', 'https://azukiazusa.dev/rss.xml'],
+	['javascriptweekly', 'https://cprss.s3.amazonaws.com/javascriptweekly.com.xml'],
 ])
